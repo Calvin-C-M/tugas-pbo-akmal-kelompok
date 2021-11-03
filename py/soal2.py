1f98b096e4daf3e93ebf74ef66fb91383d5382c2
amalianurf <amalia19006@mail.unpad.ac.id>
2021-11-03 19:43:11 +0700

    soal2.py outputnya masih kosong

--- py/soal2.py
+++ py/soal2.py
@@ -8,7 +8,7 @@ class Matriks :
     def __init__(self, b, k) :
         self.baris=b
         self.kolom=k
-        self.matriks=[[]]
+        self.matriks=[b][k]
 
     def setBaris(self,b) : self.baris=b
     def setKolom(self,k) : self.kolom=k
@@ -17,28 +17,56 @@ class Matriks :
     def getKolom(self) : return self.kolom
     
     def input(self) :
-        self.baris=input("Masukkan jumlah baris: ")
-        self.kolom=input("Masukkan jumlah kolom: ")
+        temp_row=[]
+        A = []
+        self.baris= int(input("Masukkan jumlah baris: "))
+        self.kolom= int(input("Masukkan jumlah kolom: "))
         for i in range(0, self.baris) :
             for j in range(0, self.kolom) :
-                self.matriks[i][j]=input("Masukkan baris ke-" + i + " kolom ke-" + j + " : ")
+                elemen=input("Masukkan baris ke-" + str(i+1) + " kolom ke-" + str(j+1) + " : ")
+                temp_row.append(elemen)
+            A.append(temp_row)
+            temp_row=[]
+        self.matriks = A
 
     def print(self) :
         for i in range(0, self.baris) :
             for j in range(0, self.kolom) : 
-                print(self.matriks[i][j], end="  ")
-            print("")
+                print(self.matriks[i][j], end="\t")
+            print()
 
     def getNilaiBaris(self) :
-        nilaiBaris=[]
+        jBaris = Matriks(self.baris, 0)
+        temp = []
+        nilaiBaris = 0
         for i in range(0, self.baris) : 
             for j in range(0, self.kolom) :
-                nilaiBaris[i] += self.matriks[i][j]
-        return nilaiBaris
+                nilaiBaris += int(self.matriks[i][j])
+            #print("Jumlah baris ke-" + str(i+1) + " : " + str(nilaiBaris))
+            temp.append(nilaiBaris)
+            nilaiBaris = 0
+        jBaris.matriks = temp
+        jBaris.print()
 
     def getNilaiKolom(self) :
-        nilaiKolom=[]
+        jKolom = Matriks(0, self.kolom)
+        temp = []
+        nilaiKolom = 0
         for i in range(0, self.baris) : 
             for j in range(0, self.kolom) :
-                nilaiKolom[j] += self.matriks[i][j]
-        return nilaiKolom
+                nilaiKolom += int(self.matriks[j][i])
+            #print("Jumlah kolom ke-" + str(i+1) + " : " + str(nilaiKolom))
+            temp.append(nilaiKolom)
+            nilaiKolom = 0
+        jKolom.matriks = temp
+        jKolom.print()
+
+A = Matriks(0,0)
+A.input()
+print("\nMatriks A")
+A.print()
+
+print("\nJumlah Nilai Baris Matriks A")
+A.getNilaiBaris()
+print("\nJumlah Nilai Kolom Matriks A")
+A.getNilaiKolom()
